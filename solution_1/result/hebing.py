30b0f19187e8e4aea66548f8c43865e41b0a6cbd
darong <darong@imagedt.com>
2017-05-27 14:32:30 +0800

    保存

---
+++ solution_1/result/hebing.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+baseline 2: ad.csv (creativeID/adID/camgaignID/advertiserID/appID/appPlatform) + lr
+"""
+
+import zipfile
+import pandas as pd
+from scipy import sparse
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+
+# load data
+data_root = "."
+
+dfTrain = pd.read_csv("%s/train.csv"%data_root)
+dfTest = pd.read_csv("%s/test.csv"%data_root)
+dfAd = pd.read_csv("%s/ad.csv"%data_root)
+dfUser=pd.read_csv("%s/user.csv"%data_root)
+dfuserinapp=pd.read_csv("%s/user_installedapps.csv"%data_root)
+dfposition=pd.read_csv("%s/position.csv"%data_root)
+#print (dfUser)
+
+# process data
+dfTrain = pd.merge(dfTrain, dfAd, on="creativeID")
+dfTrain = pd.merge(dfTrain, dfUser, on="userID")
+#dfTrain = pd.merge(dfTrain, dfuserinapp, on="userID")
+#dfTrain = pd.merge(dfTrain, dfposition, on="positionID")
+#print (dfTrain)
+
+dfTest = pd.merge(dfTest, dfAd, on="creativeID")
+dfTest = pd.merge(dfTest, dfUser, on="userID")
+y_train = dfTrain["label"].values
+
+# feature engineering/encoding
+enc = OneHotEncoder()
+feats = ["creativeID", "adID", "camgaignID", "advertiserID", "appID", "appPlatform","userID","age","gender","education"]
+#         "marriageStatus","haveBaby","hometown","residence"]
+for i,feat in enumerate(feats):
+    x_train = enc.fit_transform(dfTrain[feat].values.reshape(-1, 1))
+    x_test = enc.transform(dfTest[feat].values.reshape(-1, 1))
+    if i == 0:
+        X_train, X_test = x_train, x_test
+    else:
+        X_train, X_test = sparse.hstack((X_train, x_train)), sparse.hstack((X_test, x_test))#横向合并所有的ad广告
+    print(X_test)
+
+# model training
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+proba_test = lr.predict_proba(X_test)[:,1]#
+
+# submission
+df = pd.DataFrame({"instanceID": dfTest["instanceID"].values, "proba": proba_test})
+df.sort_values("instanceID", inplace=True)
+df.to_csv("submission.csv", index=False)
+with zipfile.ZipFile("submission.zip", "w") as fout:
+    fout.write("submission.csv", compress_type=zipfile.ZIP_DEFLATED)
